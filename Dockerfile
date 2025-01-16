094580f1d3ab621e96125c40d76eb20371b25135
Semyon Kozlov <semkozloff@gmail.com>
2025-01-16 19:50:52 +0400

    C++ devcontainer: clang-11, CMake, Boost

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+ARG VARIANT
+FROM mcr.microsoft.com/vscode/devcontainers/cpp:0-${VARIANT}
+
+# tzdata confirmation override
+ENV DEBIAN_FRONTEND=noninteractive 
+
+RUN apt-get update && \
+    apt-get install -y libboost-all-dev && \
+    rm -rf /var/lib/apt/lists/*
+
+# RUN git clone --recurse-submodules https://github.com/uNetworking/uWebSockets && \
+#     cd uWebSockets && \
+#     make && \
+#     make install && \
+#     rm -rf /uWebSockets
---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+
+#include <iostream>
+#include <memory>
+
+namespace beast = boost::beast;
+namespace ws = beast::websocket;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+
+void doSession(tcp::socket socket, net::yield_context yield) {
+  try {
+    ws::stream<tcp::socket> ws{std::move(socket)};
+
+    // Accept the WebSocket handshake
+    ws.async_accept(yield);
+
+    beast::flat_buffer buffer;
+
+    for (;;) {
+      // Read a message
+      ws.async_read(buffer, yield);
+
+      // Echo the message back
+      ws.text(ws.got_text());
+      ws.async_write(buffer.data(), yield);
+
+      // Clear the buffer
+      buffer.consume(buffer.size());
+    }
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+}
+
+void doListen(net::io_context& ioc, tcp::endpoint endpoint, net::yield_context yield) {
+  tcp::acceptor acceptor{ioc};
+
+  // Open the acceptor
+  acceptor.open(endpoint.protocol());
+
+  // Bind to the server address
+  acceptor.bind(endpoint);
+
+  // Start listening for connections
+  acceptor.listen();
+
+  for (;;) {
+    beast::error_code ec;
+    tcp::socket socket{ioc};
+    acceptor.async_accept(socket, yield[ec]);
+    if (!ec) {
+      net::spawn(
+        ioc, [socket = std::move(socket)](net::yield_context yield) mutable { doSession(std::move(socket), yield); });
+    }
+  }
+}
+
+int main() {
+  try {
+    const auto address = net::ip::make_address("0.0.0.0");
+    unsigned short port = 5600;
+
+    net::io_context ioc;
+
+    // Launch the listening coroutine
+    net::spawn(ioc, [&ioc, ep = tcp::endpoint{address, port}](auto yc) { doListen(ioc, ep, yc); });
+
+    // Run the I/O service
+    ioc.run();
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
