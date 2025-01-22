cc4a165789dc2326004d3c78a31802d571208bc2
Semyon Kozlov <semkozloff@gmail.com>
2025-01-22 15:44:32 +0000

    Boost.Fibers+Boost.ASIO

--- main.cpp
+++ main.cpp
@@ -3,76 +3,100 @@
 #include <boost/asio/spawn.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
+#include <boost/fiber/all.hpp>
+#include <boost/program_options.hpp>
 #include <nlohmann/json.hpp>
 #include <fmt/core.h>
+
+#include "round_robin.hpp"
+#include "yield.hpp"
 
 namespace beast = boost::beast;
 namespace ws = beast::websocket;
 namespace net = boost::asio;
+namespace fib = boost::fibers;
+namespace po = boost::program_options;
+
 using tcp = net::ip::tcp;
 
-void doSession(tcp::socket socket, net::yield_context yield) {
+void doSession(tcp::socket socket) {
   try {
     ws::stream<tcp::socket> ws{std::move(socket)};
 
     // Accept the WebSocket handshake
-    ws.async_accept(yield);
+    ws.async_accept(boost::fibers::asio::yield);
 
     beast::flat_buffer buffer;
 
     for (;;) {
       // Read a message
-      ws.async_read(buffer, yield);
+      ws.async_read(buffer, boost::fibers::asio::yield);
 
       // Echo the message back
       ws.text(ws.got_text());
-      ws.async_write(buffer.data(), yield);
+      ws.async_write(buffer.data(), boost::fibers::asio::yield);
 
       // Clear the buffer
       buffer.consume(buffer.size());
     }
   } catch (const std::exception& e) {
-    fmt::println(stderr, "Error: {}", e.what());
+    fmt::println("Error: {}", e.what());
   }
 }
 
-void doListen(net::io_context& ioc, tcp::endpoint endpoint, net::yield_context yield) {
-  tcp::acceptor acceptor{ioc};
+void doListen(net::io_context& ioc, tcp::endpoint endpoint) {
+  fmt::println("Listening fiber started");
 
-  // Open the acceptor
-  acceptor.open(endpoint.protocol());
+  try {
+    tcp::acceptor acceptor{ioc};
 
-  // Bind to the server address
-  acceptor.bind(endpoint);
+    fmt::println("Open the acceptor");
+    acceptor.open(endpoint.protocol());
 
-  // Start listening for connections
-  acceptor.listen();
+    fmt::println("Bind to the server address");
+    acceptor.bind(endpoint);
 
-  for (;;) {
-    beast::error_code ec;
-    tcp::socket socket{ioc};
-    acceptor.async_accept(socket, yield[ec]);
-    if (!ec) {
-      net::spawn(
-        ioc, [socket = std::move(socket)](net::yield_context yield) mutable { doSession(std::move(socket), yield); });
+    fmt::println("Start listening for connections");
+    acceptor.listen();
+
+    for (;;) {
+      beast::error_code ec;
+      tcp::socket socket{ioc};
+      acceptor.async_accept(socket, boost::fibers::asio::yield[ec]);
+      if (!ec) {
+        fib::fiber(doSession, std::move(socket)).detach();
+      } else {
+        fmt::println("Error: {}", ec.message());
+      }
     }
+  } catch (const std::exception& e) {
+    fmt::print("Error: {}", e.what());
   }
 }
 
-int main() {
+int main(int argc, char** argv) {
   try {
     const auto address = net::ip::make_address("0.0.0.0");
-    unsigned short port = 5600;
+    unsigned short port = 0;
 
-    net::io_context ioc;
+    po::options_description desc{"Allowed options"}
+    desc.add_options()
+      ("port,p", po::value<unsigned short>(&port)->default_value(5600))
+    ;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
 
-    // Launch the listening coroutine
-    net::spawn(ioc, [&ioc, ep = tcp::endpoint{address, port}](auto yc) { doListen(ioc, ep, yc); });
+    auto ioc = std::make_shared<net::io_context>();
+    fib::use_scheduling_algorithm<boost::fibers::asio::round_robin>(ioc);
 
-    // Run the I/O service
-    ioc.run();
+    fmt::println("Launch listening fiber");
+    fib::fiber(doListen, std::ref(*ioc), tcp::endpoint{address, port}).detach();
+
+    fmt::println("Listening on port {}", port);
+    ioc->run();
   } catch (const std::exception& e) {
-    fmt::println(stderr, "Error: {}", e.what());
+    fmt::println("Error: {}", e.what());
     return EXIT_FAILURE;
   }
 
