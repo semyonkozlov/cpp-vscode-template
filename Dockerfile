93f35926b11ceaf628774b1352a5ae8a7e5652b6
Semyon Kozlov <semkozloff@gmail.com>
2025-01-31 17:30:11 +0000

    fibers -> boost.cobalt

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,15 @@ FROM mcr.microsoft.com/vscode/devcontainers/cpp:${VARIANT}
 # tzdata confirmation override
 ENV DEBIAN_FRONTEND=noninteractive 
 
-RUN apt-get update && \
-    apt-get install -y libboost-all-dev && \
-    rm -rf /var/lib/apt/lists/*
+ARG BOOST_VERSION=1.87.0
+
+RUN cd /opt && \
+    BOOST_VERSION_MOD=$(echo $BOOST_VERSION | tr . _) && \
+    wget https://archives.boost.io/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_MOD}.tar.bz2 && \
+    tar --bzip2 -xf boost_${BOOST_VERSION_MOD}.tar.bz2 && \
+    cd boost_${BOOST_VERSION_MOD} && \
+    ./bootstrap.sh --with-toolset=clang --prefix=/usr/local && \
+    ./b2 cxxflags="-std=c++20" install
 
 # RUN git clone --recurse-submodules https://github.com/uNetworking/uWebSockets && \
 #     cd uWebSockets && \
--- main.cpp
+++ main.cpp
@@ -1,109 +1,54 @@
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/spawn.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
-#include <boost/fiber/all.hpp>
-#include <boost/program_options.hpp>
-#include <nlohmann/json.hpp>
-#include <fmt/core.h>
+#include <boost/cobalt.hpp>
+#include <boost/cobalt/main.hpp>
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
 
-#include "round_robin.hpp"
-#include "yield.hpp"
-
+namespace cobalt = boost::cobalt;
 namespace beast = boost::beast;
 namespace ws = beast::websocket;
-namespace net = boost::asio;
-namespace fib = boost::fibers;
-namespace po = boost::program_options;
+using boost::asio::detached;
+using boost::asio::ip::tcp;
+using tcp_acceptor = cobalt::use_op_t::as_default_on_t<tcp::acceptor>;
+using tcp_socket = cobalt::use_op_t::as_default_on_t<tcp::socket>;
 
-using tcp = net::ip::tcp;
-
-void doSession(tcp::socket socket) {
-  static int total_sessions = 0;
-
-  total_sessions++;
-  fmt::println("Start new session, total: {}", total_sessions);
+cobalt::promise<void> session(tcp_socket socket) {
   try {
-    ws::stream<tcp::socket> ws{std::move(socket)};
-
-    // Accept the WebSocket handshake
-    ws.async_accept(boost::fibers::asio::yield);
+    ws::stream<tcp_socket> ws{std::move(socket)};
+    co_await ws.async_accept();
 
     beast::flat_buffer buffer;
-
     for (;;) {
-      // Read a message
-      ws.async_read(buffer, boost::fibers::asio::yield);
-
-      // Echo the message back
+      std::size_t n = co_await ws.async_read(buffer);
       ws.text(ws.got_text());
-      ws.async_write(buffer.data(), boost::fibers::asio::yield);
-
-      // Clear the buffer
+      co_await ws.async_write(buffer.data());
       buffer.consume(buffer.size());
     }
-  } catch (const std::exception& e) {
-    total_sessions--;
-    fmt::println("Finish session: {}, total: {}", e.what(), total_sessions);
+  } catch (std::exception& e) {
+    std::printf("echo: exception: %s\n", e.what());
   }
 }
-
-void doListen(net::io_context& ioc, tcp::endpoint endpoint) {
-  fmt::println("Listening fiber started");
-
-  try {
-    tcp::acceptor acceptor{ioc};
-
-    fmt::println("Open the acceptor");
-    acceptor.open(endpoint.protocol());
-
-    fmt::println("Bind to the server address");
-    acceptor.bind(endpoint);
-
-    fmt::println("Start listening for connections");
-    acceptor.listen();
-
-    for (;;) {
-      boost::system::error_code ec;
-      tcp::socket socket{ioc};
-      acceptor.async_accept(socket, boost::fibers::asio::yield[ec]);
-      if (!ec) {
-        fib::fiber(doSession, std::move(socket)).detach();
-      } else {
-        fmt::println("Accept error: {}", ec.message());
-      }
-    }
-  } catch (const std::exception& e) {
-    fmt::print("Error: {}", e.what());
+cobalt::generator<tcp_socket> listen() {
+  tcp_acceptor acceptor({co_await cobalt::this_coro::executor}, {tcp::v4(), 5600});
+  for (;;) {
+    tcp_socket sock = co_await acceptor.async_accept();
+    co_yield std::move(sock);
+  }
+  co_return tcp_socket{acceptor.get_executor()};
+}
+cobalt::promise<void> run_server(cobalt::wait_group& workers) {
+  auto l = listen();
+  while (true) {
+    if (workers.size() == 10u)
+      co_await workers.wait_one();
+    else
+      workers.push_back(session(co_await l));
   }
 }
+// end::run_server[]
 
-int main(int argc, char** argv) {
-  try {
-    const auto address = net::ip::make_address("0.0.0.0");
-    unsigned short port = 0;
-
-    po::options_description desc{"Allowed options"};
-    desc.add_options()
-      ("port,p", po::value<unsigned short>(&port)->default_value(5600))
-    ;
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-
-    auto ioc = std::make_shared<net::io_context>();
-    fib::use_scheduling_algorithm<boost::fibers::asio::round_robin>(ioc);
-
-    fmt::println("Launch listening fiber");
-    fib::fiber(doListen, std::ref(*ioc), tcp::endpoint{address, port}).detach();
-
-    fmt::println("Listening on port {}", port);
-    ioc->run();
-  } catch (const std::exception& e) {
-    fmt::println("Error: {}", e.what());
-    return EXIT_FAILURE;
-  }
-
-  return EXIT_SUCCESS;
+// tag::main[]
+cobalt::main co_main(int argc, char** argv) {
+  co_await cobalt::with(cobalt::wait_group(), &run_server);
+  co_return 0u;
 }
+// end::main[]
